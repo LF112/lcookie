b1c79f78e10a90296f7be8e483177ee65ca61e32
“LF112” <lf@lf112.net>
2019-03-11 19:11:59 +0800

    Initial commit

---
+++ source/lcookie.js
@@ -0,0 +1,15 @@
+let lcookie = function( Config ){
+
+    console.log('\n %c 🍪lcookie %c https://github.com/LF112/lcookie %c BY%c LF112  \n\n', 'color: #ffffff; background: rgb(0, 145, 228); padding:5px 0;', 'background:rgba(197, 197, 197, 0.89); padding:5px 0;', 'color: #ffffff; background: rgba(49, 49, 49, 0.85); padding:5px 0;', 'color: rgb(0, 145, 228); background: rgba(49, 49, 49, 0.85); padding:5px 0;');
+
+    return new Promise( function() {
+
+        // Main
+        let defaultOption = [
+            []
+        ];
+
+        //Coding more...
+
+    });
+};
